f883bafb8a03df0f8790185ff9aca0437cc964aa
Tomasz Nowacki <tomasz.nowacki@wsb.poznan.pl>
2024-10-13 12:18:12 +0200

    Dodanie Breeze Auth

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,11 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('start');
 
 Route::get('/kontakt', function () {
     return view('kontakt');
-});
+})->name('kontakt');
 Route::get('/onas', function () {
     $zadania = [
         'Zadanie 1',
@@ -27,4 +28,16 @@ Route::get('/onas', function () {
         'Zadanie 3'
     ];
     return view('onas', ['zadania' => $zadania]);
+})->name('onas');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
